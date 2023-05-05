1e22681b37a28147c571d137b35c14183662bc0b
海屿有燕 <96456728+ZoruaFox@users.noreply.github.com>
2023-05-06 00:15:29 +0800

    Update Common.js

--- src/Common.js
+++ src/Common.js
@@ -6,7 +6,7 @@
  * _addText: '{{Gadget Header|license=CC-BY-SA-4.0|attribute=求闻百科}}'
  */
 /**
- * 这里的脚本仅为所有桌面版用户加载。
+ * 这里的脚本为所有桌面版用户加载。
  * 作为小工具加载，请编辑[[MediaWiki:Gadget-SiteCommon.js]]。
  * 为登录用户加载，请编辑[[MediaWiki:Group-user.js]]。
  */
