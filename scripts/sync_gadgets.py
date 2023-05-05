db21cf6532b8d9e4b7f80d9c5ef6cf4305963da7
KevinMei-Github <kevinmei2019@outlook.com>
2023-05-05 23:49:51 +0800

    sync_gadgets.py

---
+++ scripts/sync_gadgets.py
@@ -0,0 +1,37 @@
+import os
+
+import mwclient
+
+# Directories of files that will be checked for updates
+# "src" not included
+DIR = ["", "Gadget", "Group"]
+
+def sync_file(site: mwclient.Site, page_name: str, page_text: str):
+    page = site.pages[page_name]
+    text_old = page.text()
+    if text_old != page_text:
+        summary = "Git更新：代码仓库同步更新"
+        page.edit(text_new, summary)
+        print(page_name, "\t", "changed")
+    else:
+        print(page_name, "\t", "not_changed")
+
+site = mwclient.Site("wiki.zorua.top", scheme="https", path="/")
+site.login("Github-bot", os.environ["MW_BOT_PASSWORD"])
+
+for dir in DIR:
+    dir_whole = os.path.join("src", dir)
+    for file_name in os.listdir(dir_whole):
+        file_whole_name = os.path.join(dir_whole, file_name)
+        if os.path.isfile(file_whole_name) and (
+            file_whole_name.endswith(".css") or file_whole_name.endswith(".js")
+        ):
+            with open(file_whole_name, "r", encoding="utf-8") as pfile:
+                text_new = pfile.read()
+
+            if dir:
+                page_name = f"MediaWiki:{dir}-{file_name}"
+            else:
+                page_name = f"MediaWiki:{file_name}"
+            
+            sync_file(site, page_name, text_new)
