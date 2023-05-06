33deaca5d3f8312a6bb680ab37ba866895a7ac44
海屿有燕 <96456728+ZoruaFox@users.noreply.github.com>
2023-05-06 11:35:23 +0800

    Update Wikiplus-load.js

--- src/Gadget/Wikiplus-load.js
+++ src/Gadget/Wikiplus-load.js
@@ -1,2 +1,12 @@
+/**
+ * SPDX-License-Identifier: GPL-3.0
+ * _addText: '{{Gadget Header|license=GPL-3.0}}'
+ *
+ * @author Bhsd <https://github.com/bhsd-harry>
+ * @author 机智的小鱼君 <https://github.com/Dragon-Fish>
+ * @source <https://github.com/bhsd-harry/Wikiplus-highlight/blob/main/main.js>
+ */
+
+'use strict'
 mw.loader.load('https://cdn.jsdelivr.net/npm/wikiplus-core@latest/dist/Main.js');
 mw.loader.load('/index.php?title=MediaWiki:Gadget-Wikiplus-highlight.js&action=raw&ctype=text/javascript&maxage=86400&smaxage=86400');
