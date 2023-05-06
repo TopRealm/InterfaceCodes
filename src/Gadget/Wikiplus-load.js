ae1dc3621ef88a7db0877bac11db8959fe6f9234
海屿有燕 <96456728+ZoruaFox@users.noreply.github.com>
2023-05-06 11:12:15 +0800

    Update Wikiplus-load.js

--- src/Gadget/Wikiplus-load.js
+++ src/Gadget/Wikiplus-load.js
@@ -1,1 +1,2 @@
 mw.loader.load('https://cdn.jsdelivr.net/npm/wikiplus-core@latest/dist/Main.js');
+mw.loader.load('/index.php?title=MediaWiki:Gadget-Wikiplus-highlight.js&action=raw&ctype=text/javascript&maxage=86400&smaxage=86400');
