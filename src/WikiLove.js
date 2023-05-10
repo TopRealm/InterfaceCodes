eaee6d7eba8d1338e36b43abc82e563db696f923
海屿有燕 <96456728+ZoruaFox@users.noreply.github.com>
2023-05-10 15:14:16 +0800

    维基友爱配置

---
+++ src/WikiLove.js
@@ -0,0 +1,17 @@
+"use strict";
+
+
+/* <nowiki> */
+/** 放置于这里的JavaScript定制WikiLove，请见https://www.mediawiki.org/wiki/Special:MyLanguage/Extension:WikiLove#Custom_configuration */
+/*
+* _addText: '{{Gadget Header|license=CC-BY-SA-4.0}}'
+*/
+
+
+// 删除所有wikilove默认配置
+delete $.wikiLoveOptions.types.barnstar;
+delete $.wikiLoveOptions.types.kitten;
+delete $.wikiLoveOptions.types.food;
+
+
+/* </nowiki> */
