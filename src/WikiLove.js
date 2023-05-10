e307b77acb7de90606d165d5fe4799b835c82c19
海屿有燕 <96456728+ZoruaFox@users.noreply.github.com>
2023-05-10 15:34:25 +0800

    修正表述

--- src/WikiLove.js
+++ src/WikiLove.js
@@ -4,7 +4,7 @@
 /* <nowiki> */
 /** 放置于这里的JavaScript定制WikiLove，请见https://www.mediawiki.org/wiki/Special:MyLanguage/Extension:WikiLove#Custom_configuration */
 /*
-* _addText: '{{Gadget Header|license=CC-BY-SA-4.0}}'
+* _addText: '{{Gadget Header|license=CC-BY-SA-4.0|import=no}}'
 */
 
 
