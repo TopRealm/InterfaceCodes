bbd56aca22d4206c5f95b0d9ba41bdab3444d99d
海屿有燕 <96456728+ZoruaFox@users.noreply.github.com>
2023-05-13 21:59:18 +0800

    Update Fullwidth-search-fix.js

--- src/Gadget/Fullwidth-search-fix.js
+++ src/Gadget/Fullwidth-search-fix.js
@@ -2,32 +2,35 @@
  * SPDX-License-Identifier: CC-BY-SA-4.0
  * _addText: '{{Gadget Header|license=CC-BY-SA-4.0}}'
  *
- * @source <zh.wikipedia.org/wiki/MediaWiki:Gadget-fullwidth-search-fix.js>
+ * @base <https://zh.wikipedia.org/wiki/MediaWiki:Gadget-fullwidth-search-fix.js>
+ * @source <https://git.qiuwen.wiki/InterfaceAdmin/Gadgets/src/branch/master/src/Gadgets/Fullwidth-search-fix>
  * @dependency ext.gadget.i18n, mediawiki.util
  */
 /* <nowiki> */
 'use strict';
 
-(function FullwidthSearchFix() {
+$(function fullWidthSearchFix() {
   // don't be too aggresive. just fix some obvious typos
   var searchTerm = mw.util.getParamValue('search');
   if (searchTerm !== null && mw.util.getParamValue('fulltext') === null) {
     // namespace names
-    var colonIdx = searchTerm.indexOf('：');
-    if (colonIdx !== -1) {
-      var ns = searchTerm.slice(0, Math.max(0, colonIdx));
-      var page = searchTerm.slice(Math.max(0, colonIdx + 1));
-      if (mw.config.get('wgNamespaceIds')[ns.toLowerCase()]) {
+    var colonIndex = searchTerm.indexOf('：');
+    if (colonIndex !== -1) {
+      var namespace = searchTerm.slice(0, Math.max(0, colonIndex));
+      var pageName = searchTerm.slice(Math.max(0, colonIndex + 1));
+      if (mw.config.get('wgNamespaceIds')[namespace.toLowerCase()]) {
         // valid namespace
-        var url = "".concat(mw.config.get('wgScript'), "?search=").concat(encodeURIComponent("".concat(ns, ":").concat(page)));
-        var localize = window.i18n.localize;
+        var url = "".concat(mw.config.get('wgScript'), "?search=").concat(encodeURIComponent("".concat(namespace, ":").concat(pageName)));
+        var localize = i18n.localize;
         mw.notify("".concat(localize({
           'zh-hans': '重定向',
           'zh-hant': '重新導向'
-        }), "\u81F3").concat(mw.html.escape(url)));
-        window.location.href = url;
+        }), "\u81F3").concat(mw.html.escape(url)), {
+          type: 'success'
+        });
+        location.href = url;
       }
     }
   }
-})();
+});
 /* </nowiki> */
