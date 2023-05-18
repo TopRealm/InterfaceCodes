06d300e9e98de62d44cfc8534607732772bbf649
海屿有燕 <96456728+ZoruaFox@users.noreply.github.com>
2023-05-19 00:55:29 +0800

    Update CookieWarning.js

--- src/Gadgets/Compatibility/CookieWarning/CookieWarning.js
+++ src/Gadgets/Compatibility/CookieWarning/CookieWarning.js
@@ -36,8 +36,8 @@ $(function cookieWarning() {
       cookieNotice: localize({
         en: 'To provide better experience, we utilize cookies and other technologies to provide services. You can refer to our <a rel="noopener" target="_blank" href="/wiki/LIB:PIPP?consentRead=1" title="Personal Information Protection Policy"><em>Personal Information Protection Policy</em></a> to learn how we handle your personal information.',
         ja: 'より良いサービスを提供するために、クッキーやその他の技術を利用しています。個人情報の取り扱い方法については、<a rel="noopener" target="_blank" href="/wiki/LIB:PIPP?consentRead=1" title="個人情報保護条例">個人情報保護条例</a>をご参照ください。',
-        'zh-hans': '为了提供最佳体验，我们利用Cookie及其他技术提供服务；您可以查阅我们的<a rel="noopener" target="_blank" href="/wiki/LIB:PIPP?consentRead=1" title="个人信息保护方针">个人信息保护条例</a>，了解我们如何处理您的个人信息。',
-        'zh-hant': '為了提供最佳體驗，我们利用Cookie及其他技術提供服務；您可以查閱我們的<a rel="noopener" target="_blank" href="/wiki/LIB:PIPP?consentRead=1" title="個人信息保護方針">個人信息保護条例</a>，了解我們如何處理您的個人信息'
+        'zh-hans': '为了提供最佳体验，我们利用Cookie及其他技术提供服务；您可以查阅我们的<a rel="noopener" target="_blank" href="/wiki/LIB:PIPP?consentRead=1" title="个人信息保护条例">个人信息保护条例</a>，了解我们如何处理您的个人信息。',
+        'zh-hant': '為了提供最佳體驗，我们利用Cookie及其他技術提供服務；您可以查閱我們的<a rel="noopener" target="_blank" href="/wiki/LIB:PIPP?consentRead=1" title="個人信息保護条例">個人信息保護条例</a>，了解我們如何處理您的個人信息'
       }),
       'I agree': localize({
         ja: '同意する',
