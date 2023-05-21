96cd2cbb94231a2f206a78d809570125f9a23eb0
海屿有燕 <96456728+ZoruaFox@users.noreply.github.com>
2023-05-21 23:21:17 +0800

    Create Citizen.js (#15)

---
+++ src/others/Citizen.js
@@ -0,0 +1,13 @@
+'use strict'
+/* 这里的任何JavaScript将为使用Citizen皮肤的用户加载 */
+mw.loader.using( [ 'mediawiki.util' ] ).done( function() {
+	/* Trigger search box when click on the fake search button on main page */
+	if ( mw.config.get( 'wgPageName' ) === '有兽档案馆:首页' ) {
+		document.getElementById( 'skin-citizen-search-trigger' ).addEventListener( 'click', function() {
+			var event = new Event( 'input', { bubbles: true, composed: true } ),
+				checkbox = document.getElementById( 'citizen-search__checkbox' );
+			checkbox.checked = true;
+			checkbox.dispatchEvent( event );
+		} );
+	}
+} );
