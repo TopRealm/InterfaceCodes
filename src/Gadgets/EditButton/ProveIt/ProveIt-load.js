0ca9b1c751ed113f7c358f74e29922def8de463c
ZoruaFox <96456728+ZoruaFox@users.noreply.github.com>
2023-06-25 03:42:44 +0800

    Update ProveIt-load.js

--- src/Gadgets/EditButton/ProveIt/ProveIt-load.js
+++ src/Gadgets/EditButton/ProveIt/ProveIt-load.js
@@ -18,7 +18,7 @@ var loadProveIt = function loadProveIt() {
     "proveit-tag": "ProveIt_edit",
     "proveit-summary": "使用[[MediaWiki:Gadget-ProveIt.js|ProveIt]]编辑参考文献",
     "proveit-templates": ["Citation", "Cite arXiv", "Cite AV media", "Cite AV media notes", "Cite book", "Cite bioRxiv", "Cite conference", "Cite comic", "Cite encyclopedia", "Cite episode", "Cite interview", "Cite journal", "Cite magazine", "Cite mailing list", "Cite map", "Cite news", "Cite newsgroup", "Cite paper", "Cite podcast", "Cite press release", "Cite report", "Cite serial", "Cite sign", "Cite speech", "Cite techreport", "Cite thesis", "Cite tweet", "Cite video", "Cite video game", "Cite ssrn", "Cite web"],
-    "proveit-namespaces": [0, 2, 118 // Draft namespace
+    "proveit-namespaces": [0, 2, 300, 302 // Draft namespace
     ]
   });
 
