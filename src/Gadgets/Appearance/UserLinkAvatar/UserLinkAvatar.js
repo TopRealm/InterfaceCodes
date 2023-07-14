a5f9f8797794eb392f86592efafe67cf798414bd
ZoruaFox <96456728+ZoruaFox@users.noreply.github.com>
2023-07-14 19:48:18 +0800

    Update UserLinkAvatar.js
    抄错代码的人，脑子一定很缺脑子吧

--- src/Gadgets/Appearance/UserLinkAvatar/UserLinkAvatar.js
+++ src/Gadgets/Appearance/UserLinkAvatar/UserLinkAvatar.js
@@ -21,67 +21,68 @@
 
 /* </nowiki> */
 
+/* <pre> */
 "use strict";
-// <pre>
-(() => {
-    const magnifierOn = +mw.user.options.get("gadget-userLinkAvatarMagnifier", 0) === 1;
-    const $window = $(window);
-    const loadingImage = "https://wiki.zorua.top/images/d/de/Ajax-loader.gif";
-    $window.on("load.UserLinkAvatar", () => {
-        const images = [];
-        $(".mw-userlink:not(.user-avatar-added)").each((_, ele) => {
-            const item = $(ele);
-            const src = `https://wiki.zorua.top/extensions/Avatar/avatar.php?user=${encodeURIComponent(item.text())}`;
-            const img = $("<img/>").on("error", () => {
-                window.setTimeout(() => {
+(function () {
+    var magnifierOn = +mw.user.options.get("gadget-userLinkAvatarMagnifier", 0) === 1;
+    var $window = $(window);
+    var loadingImage = "https://img.moegirl.org.cn/common/d/d1/Windows_10_loading.gif";
+    $window.on("load.UserLinkAvatar", function () {
+        var images = [];
+        $(".mw-userlink:not(.user-avatar-added)").each(function (_, ele) {
+            var item = $(ele);
+            var src = "https://commons.moegirl.org.cn/extensions/Avatar/avatar.php?user=".concat(encodeURIComponent(item.text()));
+            var img = $("<img/>").on("error", function () {
+                window.setTimeout(function () {
                     img.closest(".userlink-avatar").remove();
                 }, 0);
             }).addClass("userlink-avatar-small").attr({
                 "data-src": src,
-                src: loadingImage,
+                src: loadingImage
             });
             images.push(img[0]);
-            const bigAvatar = $("<span/>").addClass("userlink-avatar");
+            var bigAvatar = $("<span/>").addClass("userlink-avatar");
             item.prepend(bigAvatar.append(img));
             item.addClass("user-avatar-added");
             if (magnifierOn) {
-                const magnifierImg = $("<img/>", {
+                var magnifierImg_1 = $("<img/>", {
                     attr: {
                         "data-src": src,
-                        src: loadingImage,
+                        src: loadingImage
                     },
                     on: {
-                        error: () => {
-                            window.setTimeout(() => {
-                                magnifierImg.closest(".userlink-avatar-large").remove();
+                        error: function () {
+                            window.setTimeout(function () {
+                                magnifierImg_1.closest(".userlink-avatar-large").remove();
                             }, 0);
-                        },
-                    },
+                        }
+                    }
                 });
-                images.push(magnifierImg[0]);
-                bigAvatar.on("click", () => {
-                    window.open(`https://wiki.zorua.top/index.php?title=Special%3A查看头像&user=${encodeURIComponent(item.text())}`, "_blank");
+                images.push(magnifierImg_1[0]);
+                bigAvatar.on("click", function () {
+                    window.open("https://commons.moegirl.org.cn/index.php?title=Special%3A\u67E5\u770B\u5934\u50CF&user=".concat(encodeURIComponent(item.text())), "_blank");
                     return false;
                 }).append($("<div/>", {
                     attr: {
-                        "class": "userlink-avatar-large",
-                    },
-                }).prepend(magnifierImg)).addClass("userlink-avatar-hover");
+                        "class": "userlink-avatar-large"
+                    }
+                }).prepend(magnifierImg_1)).addClass("userlink-avatar-hover");
                 item.before(bigAvatar);
-                bigAvatar.add(bigAvatar.children()).attr("title", `查看用户${item.text()}的头像`);
+                bigAvatar.add(bigAvatar.children()).attr("title", "\u67E5\u770B\u7528\u6237".concat(item.text(), "\u7684\u5934\u50CF"));
             }
         });
         if (typeof window.lazyload === "function") {
             window.lazyload(images);
-        } else {
-            images.forEach((ele) => {
+        }
+        else {
+            images.forEach(function (ele) {
                 ele.src = ele.dataset.src;
             });
         }
     });
-    $(() => {
+    $(function () {
         $window.trigger("load.UserLinkAvatar");
     });
 })();
-// </pre>
-
+ 
+/* </pre> */
