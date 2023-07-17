9456cd1459ac0802bbc22676bb46cb6f6df7cd60
Blossomstripe <16372095+Blossomstripe@users.noreply.github.com>
2023-07-17 22:46:41 +0800

    Update UserLinkAvatar; Upload jQueryLazyload

--- src/Gadgets/Appearance/UserLinkAvatar/UserLinkAvatar.js
+++ src/Gadgets/Appearance/UserLinkAvatar/UserLinkAvatar.js
@@ -1,10 +1,10 @@
-/* <nowiki> */
 /**
- * SPDX-License-Identifier: CC-BY-SA-4.0
- * _addText: '{{Gadget Header|license=CC-BY-SA-4.0|import=no}}'
- * @source <https://github.com/TopRealm/InterfaceCodes/blob/master/src/Gadgets/Appearance/UserLinkAvatar/UserLinkAvatar.css>
- * @author ZoruaFox <zoruaa@vip.qq.com>.
-*/
+ * SPDX-License-Identifier: CC BY-NC-SA 3.0 CN
+ * _addText: '{{Gadget Header|license=cc-by-nc-sa-3.0-cn}}'
+ *
+ * @source <https://zh.moegirl.org.cn/MediaWiki:Gadget-UserLinkAvatar.js>
+ * @author Moegirlpedia contributors, ColGem <https://wiki.zorua.top/wiki/User:ColGem>
+ */
 /**
  * +--------------------------------------------------------+
  * |         === WARNING: GLOBAL GADGET FILE ===            |
@@ -12,13 +12,70 @@
  * |      All changes should be made in the repository,     |
  * |              otherwise they will be lost.              |
  * +--------------------------------------------------------+
- * |        Changes to this page affect many users.         |
- * |  Please discuss changes at Talk page before editing.   |
+ * |      Changes to this page may affect many users.       |
+ * |  Please discuss changes at talk page before editing.   |
  * +--------------------------------------------------------+
  */
-
-$('.mw-userlink').each(function(_, item) {
-	item = $(item);
-	item.prepend($('<img/>').addClass('userlink-avatar').attr('src', mw.config.get('wgScriptPath') + '/extensions/Avatar/avatar.php?user=' + item.text()));
-});
+/* <nowiki> */
+"use strict";
+(function () {
+  var magnifierOn = +mw.user.options.get("gadget-userLinkAvatarMagnifier", 0) === 1;
+  var $window = $(window);
+  var loadingImage = "https://wiki.zorua.top/images/b/b1/Loading_icon.gif";
+  $window.on("load.UserLinkAvatar", function () {
+    var images = [];
+    $(".mw-userlink:not(.user-avatar-added)").each(function (_, ele) {
+      var item = $(ele);
+      var src = "https://wiki.zorua.top/extensions/Avatar/avatar.php?user=".concat(encodeURIComponent(item.text()));
+      var img = $("<img/>").on("error", function () {
+        window.setTimeout(function () {
+          img.closest(".userlink-avatar").remove();
+        }, 0);
+      }).addClass("userlink-avatar-small").attr({
+        "data-src": src,
+        src: loadingImage
+      });
+      images.push(img[0]);
+      var bigAvatar = $("<span/>").addClass("userlink-avatar");
+      item.prepend(bigAvatar.append(img));
+      item.addClass("user-avatar-added");
+      if (magnifierOn) {
+        var magnifierImg_1 = $("<img/>", {
+          attr: {
+            "data-src": src,
+            src: loadingImage
+          },
+          on: {
+            error: function error() {
+              window.setTimeout(function () {
+                magnifierImg_1.closest(".userlink-avatar-large").remove();
+              }, 0);
+            }
+          }
+        });
+        images.push(magnifierImg_1[0]);
+        bigAvatar.on("click", function () {
+          window.open("https://wiki.zorua.top/index.php?title=Special%3A\u67E5\u770B\u5934\u50CF&user=".concat(encodeURIComponent(item.text())), "_blank");
+          return false;
+        }).append($("<div/>", {
+          attr: {
+            "class": "userlink-avatar-large"
+          }
+        }).prepend(magnifierImg_1)).addClass("userlink-avatar-hover");
+        item.before(bigAvatar);
+        bigAvatar.add(bigAvatar.children()).attr("title", "\u67E5\u770B\u7528\u6237".concat(item.text(), "\u7684\u5934\u50CF"));
+      }
+    });
+    if (typeof window.lazyload === "function") {
+      window.lazyload(images);
+    } else {
+      images.forEach(function (ele) {
+        ele.src = ele.dataset.src;
+      });
+    }
+  });
+  $(function () {
+    $window.trigger("load.UserLinkAvatar");
+  });
+})();
 /* </nowiki> */
---
+++ src/Gadgets/Compatibility/jQueryLazyload/jQueryLazyload.js
@@ -0,0 +1,204 @@
+/**
+ * SPDX-License-Identifier: MIT
+ * _addText: '{{Gadget Header|license=MIT}}'
+ *
+ * @source <https://zh.moegirl.org.cn/MediaWiki:Gadget-jQueryLazyload.js>
+ * @author AnnAngela <https://github.com/AnnAngela>, ColGem <https://wiki.zorua.top/wiki/User:ColGem>
+ */
+/**
+ * +--------------------------------------------------------+
+ * |         === WARNING: GLOBAL GADGET FILE ===            |
+ * +--------------------------------------------------------+
+ * |      All changes should be made in the repository,     |
+ * |              otherwise they will be lost.              |
+ * +--------------------------------------------------------+
+ * |      Changes to this page may affect many users.       |
+ * |  Please discuss changes at talk page before editing.   |
+ * +--------------------------------------------------------+
+ */
+/* <nowiki> */
+"use strict";
+var __read = void 0 && (void 0).__read || function (o, n) {
+  var m = typeof Symbol === "function" && o[Symbol.iterator];
+  if (!m) return o;
+  var i = m.call(o),
+    r,
+    ar = [],
+    e;
+  try {
+    while ((n === void 0 || n-- > 0) && !(r = i.next()).done) ar.push(r.value);
+  } catch (error) {
+    e = {
+      error: error
+    };
+  } finally {
+    try {
+      if (r && !r.done && (m = i["return"])) m.call(i);
+    } finally {
+      if (e) throw e.error;
+    }
+  }
+  return ar;
+};
+var __spreadArray = void 0 && (void 0).__spreadArray || function (to, from, pack) {
+  if (pack || arguments.length === 2) for (var i = 0, l = from.length, ar; i < l; i++) {
+    if (ar || !(i in from)) {
+      if (!ar) ar = Array.prototype.slice.call(from, 0, i);
+      ar[i] = from[i];
+    }
+  }
+  return to.concat(ar || Array.prototype.slice.call(from));
+};
+var __values = void 0 && (void 0).__values || function (o) {
+  var s = typeof Symbol === "function" && Symbol.iterator,
+    m = s && o[s],
+    i = 0;
+  if (m) return m.call(o);
+  if (o && typeof o.length === "number") return {
+    next: function next() {
+      if (o && i >= o.length) o = void 0;
+      return {
+        value: o && o[i++],
+        done: !o
+      };
+    }
+  };
+  throw new TypeError(s ? "Object is not iterable." : "Symbol.iterator is not defined.");
+};
+(function () {
+  var defaults = {
+    src: "data-src",
+    srcset: "data-srcset",
+    selector: ".lazyload",
+    root: null,
+    rootMargin: "0px",
+    threshold: 0
+  };
+  var extend = function extend(_deep) {
+    var e_1, _a;
+    var _args = [];
+    for (var _i = 1; _i < arguments.length; _i++) {
+      _args[_i - 1] = arguments[_i];
+    }
+    var extended = {};
+    var deep = typeof _deep === "boolean" ? _deep : false;
+    var args = __spreadArray(__spreadArray([], __read(typeof _deep !== "boolean" ? [_deep] : []), false), __read(_args), false);
+    var merge = function merge(obj) {
+      for (var prop in obj) {
+        if (Object.prototype.hasOwnProperty.bind(obj)(prop)) {
+          if (deep && Object.prototype.toString.bind(obj[prop])() === "[object Object]") {
+            extended[prop] = extend(true, extended[prop], obj[prop]);
+          } else {
+            extended[prop] = obj[prop];
+          }
+        }
+      }
+    };
+    try {
+      for (var args_1 = __values(args), args_1_1 = args_1.next(); !args_1_1.done; args_1_1 = args_1.next()) {
+        var obj = args_1_1.value;
+        merge(obj);
+      }
+    } catch (e_1_1) {
+      e_1 = {
+        error: e_1_1
+      };
+    } finally {
+      try {
+        if (args_1_1 && !args_1_1.done && (_a = args_1["return"])) _a.call(args_1);
+      } finally {
+        if (e_1) throw e_1.error;
+      }
+    }
+    return extended;
+  };
+  var LazyLoad = function () {
+    function LazyLoad(images, options) {
+      this.settings = extend(defaults, options || {});
+      this.images = images || document.querySelectorAll(this.settings.selector);
+      this.observer = null;
+      this.init();
+    }
+    LazyLoad.prototype.init = function () {
+      if (!window.IntersectionObserver) {
+        this.loadImages();
+        return;
+      }
+      var self = this;
+      var observerConfig = {
+        root: this.settings.root,
+        rootMargin: this.settings.rootMargin,
+        threshold: [this.settings.threshold]
+      };
+      this.observer = new IntersectionObserver(function (entries) {
+        Array.prototype.forEach.bind(entries)(function (entry) {
+          if (entry.isIntersecting) {
+            self.observer.unobserve(entry.target);
+            var src = entry.target.getAttribute(self.settings.src);
+            var srcset = entry.target.getAttribute(self.settings.srcset);
+            if ("img" === entry.target.tagName.toLowerCase()) {
+              if (src) {
+                entry.target.src = src;
+              }
+              if (srcset) {
+                entry.target.srcset = srcset;
+              }
+            } else {
+              entry.target.style.backgroundImage = "url(".concat(src, ")");
+            }
+          }
+        });
+      }, observerConfig);
+      Array.prototype.forEach.bind(this.images)(function (image) {
+        self.observer.observe(image);
+      });
+    };
+    LazyLoad.prototype.loadAndDestroy = function () {
+      if (!this.settings) {
+        return;
+      }
+      this.loadImages();
+      this.destroy();
+    };
+    LazyLoad.prototype.loadImages = function () {
+      if (!this.settings) {
+        return;
+      }
+      var self = this;
+      Array.prototype.forEach.bind(this.images)(function (image) {
+        var src = image.getAttribute(self.settings.src);
+        var srcset = image.getAttribute(self.settings.srcset);
+        if ("img" === image.tagName.toLowerCase()) {
+          if (src) {
+            image.src = src;
+          }
+          if (srcset) {
+            image.srcset = srcset;
+          }
+        } else {
+          image.style.backgroundImage = "url('".concat(src, "')");
+        }
+      });
+    };
+    LazyLoad.prototype.destroy = function () {
+      if (!this.settings) {
+        return;
+      }
+      this.observer.disconnect();
+      this.settings = null;
+    };
+    return LazyLoad;
+  }();
+  window.lazyload = function (images, options) {
+    return new LazyLoad(images, options);
+  };
+  if (window.jQuery) {
+    jQuery.fn.lazyload = function (_options) {
+      var options = _options || {};
+      options.attribute || (options.attribute = "data-src");
+      new LazyLoad(this.toArray(), options);
+      return this;
+    };
+  }
+})();
+/* </nowiki> */
