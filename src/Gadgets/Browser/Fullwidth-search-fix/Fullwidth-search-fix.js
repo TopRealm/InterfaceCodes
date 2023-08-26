c37a389af4767a79aa52e825627ee776a0a2141f
ZoruaFox <96456728+ZoruaFox@users.noreply.github.com>
2023-08-26 17:39:03 +0800

    Update Fullwidth-search-fix.js (#136)
    style: add tag for mw.notify()
    
    chore: add tag and type of mw.notify()

--- src/Gadgets/Browser/Fullwidth-search-fix/Fullwidth-search-fix.js
+++ src/Gadgets/Browser/Fullwidth-search-fix/Fullwidth-search-fix.js
@@ -3,12 +3,21 @@
  * _addText: '{{Gadget Header|license=CC-BY-SA-4.0}}'
  *
  * @base <https://zh.wikipedia.org/wiki/MediaWiki:Gadget-fullwidth-search-fix.js>
- * @source <https://git.qiuwen.wiki/InterfaceAdmin/Gadgets/src/branch/master/src/Gadgets/Fullwidth-search-fix>
+ * @source <https://git.qiuwen.wiki/InterfaceAdmin/Gadgets/src/branch/master/src/Gadgets/FullwidthSearchFix>
  * @dependency ext.gadget.i18n, mediawiki.util
  */
+/**
+ * +--------------------------------------------------------+
+ * |         === WARNING: GLOBAL GADGET FILE ===            |
+ * +--------------------------------------------------------+
+ * |      All changes should be made in the repository,     |
+ * |              otherwise they will be lost.              |
+ * +--------------------------------------------------------+
+ * |      Changes to this page may affect many users.       |
+ * |  Please discuss changes at talk page before editing.   |
+ * +--------------------------------------------------------+
+ */
 /* <nowiki> */
-'use strict';
-
 $(function fullWidthSearchFix() {
   // don't be too aggresive. just fix some obvious typos
   var searchTerm = mw.util.getParamValue('search');
@@ -21,11 +30,8 @@ $(function fullWidthSearchFix() {
       if (mw.config.get('wgNamespaceIds')[namespace.toLowerCase()]) {
         // valid namespace
         var url = "".concat(mw.config.get('wgScript'), "?search=").concat(encodeURIComponent("".concat(namespace, ":").concat(pageName)));
-        var localize = i18n.localize;
-        mw.notify("".concat(localize({
-          'zh-hans': '重定向',
-          'zh-hant': '重新導向'
-        }), "\u81F3").concat(mw.html.escape(url)), {
+        mw.notify("".concat(window.wgULS('重定向', '重新導向'), "\u81F3").concat(namespace, ":").concat(pageName), {
+          tag: 'fullWidthSearchFix',
           type: 'success'
         });
         location.href = url;
