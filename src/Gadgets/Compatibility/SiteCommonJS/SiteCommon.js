ed756de75c9261abc3e0b2eb3450b5399b39a6ba
ZoruaFox <96456728+ZoruaFox@users.noreply.github.com>
2023-10-28 00:53:18 +0800

    Update SiteCommon.js: pull

--- src/Gadgets/Compatibility/SiteCommonJS/SiteCommon.js
+++ src/Gadgets/Compatibility/SiteCommonJS/SiteCommon.js
@@ -1,193 +1,270 @@
-/* <nowiki> */
-'use strict';
-
 /**
  * SPDX-License-Identifier: CC-BY-SA-4.0
  * _addText: '{{Gadget Header|license=CC-BY-SA-4.0}}'
  *
- * @source <zh.wikipedia.org/wiki/MediaWiki:Common.js>
- * @source <en.wikipedia.org/wiki/MediaWiki:Gadget-exlinks.js>
- * @source <meta.wikimedia.org/wiki/MediaWiki:Gadget-ShortLink.js>
- * @source <www.mediawiki.org/wiki/MediaWiki:Gadget-workinprogress.js>
- * @source <www.mediawiki.org/wiki/Snippets/Load_JS_and_CSS_by_URL>
- * @dependency mediawiki.util
+ * @base <https://zh.wikipedia.org/wiki/MediaWiki:Common.js>
+ * @base <https://en.wikipedia.org/wiki/MediaWiki:Gadget-exlinks.js>
+ * @base <https://meta.wikimedia.org/wiki/MediaWiki:Gadget-ShortLink.js>
+ * @base <https://www.mediawiki.org/wiki/MediaWiki:Gadget-workinprogress.js>
+ * @base <https://www.mediawiki.org/wiki/Snippets/Load_JS_and_CSS_by_URL>
+ * @base <https://en.wikipedia.org/wiki/MediaWiki:Gadget-search-new-tab.js>
+ * @source <https://git.qiuwen.net.cn/InterfaceAdmin/Gadgets/src/branch/master/src/Gadgets/SiteCommonJS>
+ * @dependency ext.gadget.i18n, mediawiki.Title, mediawiki.util, oojs-ui-core
  */
-(function ($, mw) {
-  if (window.SiteCommonJS) {
+/**
+ * +--------------------------------------------------------+
+ * |         === WARNING: GLOBAL GADGET FILE ===            |
+ * +--------------------------------------------------------+
+ * |      All changes should be made in the repository,     |
+ * |              otherwise they will be lost.              |
+ * +--------------------------------------------------------+
+ * |      Changes to this page may affect many users.       |
+ * |  Please discuss changes at talk page before editing.   |
+ * +--------------------------------------------------------+
+ */
+/* <nowiki> */
+function _slicedToArray(arr, i) { return _arrayWithHoles(arr) || _iterableToArrayLimit(arr, i) || _unsupportedIterableToArray(arr, i) || _nonIterableRest(); }
+function _nonIterableRest() { throw new TypeError("Invalid attempt to destructure non-iterable instance.\nIn order to be iterable, non-array objects must have a [Symbol.iterator]() method."); }
+function _iterableToArrayLimit(r, l) { var t = null == r ? null : "undefined" != typeof Symbol && r[Symbol.iterator] || r["@@iterator"]; if (null != t) { var e, n, i, u, a = [], f = !0, o = !1; try { if (i = (t = t.call(r)).next, 0 === l) { if (Object(t) !== t) return; f = !1; } else for (; !(f = (e = i.call(t)).done) && (a.push(e.value), a.length !== l); f = !0); } catch (r) { o = !0, n = r; } finally { try { if (!f && null != t.return && (u = t.return(), Object(u) !== u)) return; } finally { if (o) throw n; } } return a; } }
+function _arrayWithHoles(arr) { if (Array.isArray(arr)) return arr; }
+function _createForOfIteratorHelper(o, allowArrayLike) { var it = typeof Symbol !== "undefined" && o[Symbol.iterator] || o["@@iterator"]; if (!it) { if (Array.isArray(o) || (it = _unsupportedIterableToArray(o)) || allowArrayLike && o && typeof o.length === "number") { if (it) o = it; var i = 0; var F = function F() {}; return { s: F, n: function n() { if (i >= o.length) return { done: true }; return { done: false, value: o[i++] }; }, e: function e(_e) { throw _e; }, f: F }; } throw new TypeError("Invalid attempt to iterate non-iterable instance.\nIn order to be iterable, non-array objects must have a [Symbol.iterator]() method."); } var normalCompletion = true, didErr = false, err; return { s: function s() { it = it.call(o); }, n: function n() { var step = it.next(); normalCompletion = step.done; return step; }, e: function e(_e2) { didErr = true; err = _e2; }, f: function f() { try { if (!normalCompletion && it.return != null) it.return(); } finally { if (didErr) throw err; } } }; }
+function _unsupportedIterableToArray(o, minLen) { if (!o) return; if (typeof o === "string") return _arrayLikeToArray(o, minLen); var n = Object.prototype.toString.call(o).slice(8, -1); if (n === "Object" && o.constructor) n = o.constructor.name; if (n === "Map" || n === "Set") return Array.from(o); if (n === "Arguments" || /^(?:Ui|I)nt(?:8|16|32)(?:Clamped)?Array$/.test(n)) return _arrayLikeToArray(o, minLen); }
+function _arrayLikeToArray(arr, len) { if (len == null || len > arr.length) len = arr.length; for (var i = 0, arr2 = new Array(len); i < len; i++) arr2[i] = arr[i]; return arr2; }
+$(function siteCommonJS() {
+  var _$toggle$parent$;
+  if (window.SiteCommonJSInstalled) {
     return;
   }
-  window.SiteCommonJS = true;
+  window.SiteCommonJSInstalled = true;
   /**
    * &withCSS= and &withJS= URL parameters
    * Allow to try custom scripts from MediaWiki space
    * without editing personal .css or .js files
    */
-  mw.loader.using(['mediawiki.util'], function () {
-    if (mw.util.getParamValue('withCSS') || mw.util.getParamValue('withJS') || mw.util.getParamValue('withModule')) {
-      var extraCSS = mw.util.getParamValue('withCSS');
-      var extraJS = mw.util.getParamValue('withJS');
-      var extraModule = mw.util.getParamValue('withModule');
-      if (/^MediaWiki:[^#%&<=>]*\.css$/.test(extraCSS)) {
-        mw.loader.load("".concat(mw.config.get('wgServer') + mw.config.get('wgScript'), "?title=").concat(mw.util.wikiUrlencode(extraCSS), "&action=raw&ctype=text/css&maxage=86400&smaxage=86400"), 'text/css');
-      }
-      if (/^MediaWiki:[^#%&<=>]*\.js$/.test(extraJS)) {
-        mw.loader.load("".concat(mw.config.get('wgServer') + mw.config.get('wgScript'), "?title=").concat(mw.util.wikiUrlencode(extraJS), "&action=raw&ctype=text/javascript&maxage=86400&smaxage=86400"));
-      }
-      if (/^ext\.gadget\.[^,|]+$/.test(extraModule)) {
-        mw.loader.load(extraModule);
-      }
+  if (mw.util.getParamValue('withCSS') || mw.util.getParamValue('withJS') || mw.util.getParamValue('withModule')) {
+    var extraCSS = mw.util.getParamValue('withCSS');
+    var extraJS = mw.util.getParamValue('withJS');
+    var extraModule = mw.util.getParamValue('withModule');
+    if (extraCSS && /^MediaWiki:[^#%&<=>]*\.css$/.test(extraCSS)) {
+      mw.loader.load(mw.util.getUrl(extraCSS, {
+        action: 'raw',
+        ctype: 'text/css',
+        maxage: '3600',
+        smaxage: '3600'
+      }), 'text/css');
     }
-  });
+    if (extraJS && /^MediaWiki:[^#%&<=>]*\.js$/.test(extraJS)) {
+      mw.loader.load(mw.util.getUrl(extraJS, {
+        action: 'raw',
+        ctype: 'text/javascript',
+        maxage: '3600',
+        smaxage: '3600'
+      }));
+    }
+    if (extraModule && /^ext\.[^,|]+$/.test(extraModule)) {
+      mw.loader.load(extraModule);
+    }
+  }
   /**
    * Load CSS and JS files temporarily through URL.
    * &use=File1.css|File2.css|File3.js
    */
-  mw.loader.using(['mediawiki.util'], function () {
-    var files = mw.util.getParamValue('use');
-    if (!files) {
-      return;
-    }
-    var user = mw.RegExp.escape(mw.config.get('wgUserName', ''));
-    var FileRE = new RegExp("^(?:MediaWiki:".concat(user ? "|User:".concat(user, "/") : '', ")[^&<>=%#]*\\.(js|css)$"));
-    var ExtRE = new RegExp('^ext\\.[^,]+$');
-    var path = "".concat(mw.config.get('wgServer') + mw.config.get('wgScript'), "?action=raw&ctype=text/");
-    files.split('|').forEach(function (k, v) {
-      var f = v.toString().trim();
-      var what = FileRE.exec(f) || ['', ''];
-      switch (what[1]) {
-        case 'js':
-          mw.loader.load("".concat(path, "javascript&title=").concat(encodeURIComponent(f)));
-          break;
-        case 'css':
-          mw.loader.load("".concat(path, "css&title=").concat(encodeURIComponent(f)));
-          break;
-        default:
-          if (ExtRE.exec(f)) {
-            mw.loader.load(f);
-          }
+  var useFiles = mw.util.getParamValue('use');
+  if (useFiles) {
+    var _mw$config$get;
+    var wgUserName = mw.util.escapeRegExp((_mw$config$get = mw.config.get('wgUserName')) !== null && _mw$config$get !== void 0 ? _mw$config$get : '');
+    var FileRegex = new RegExp("^(?:MediaWiki:".concat(wgUserName ? "|User:".concat(wgUserName, "/") : '', ")[^&<>=%#]*\\.(js|css)$"));
+    var ExtRegex = /^ext\.[^,]+$/;
+    var path = "".concat(mw.config.get('wgScript'), "?action=raw&ctype=text/");
+    var _iterator = _createForOfIteratorHelper(useFiles.split('|').entries()),
+      _step;
+    try {
+      for (_iterator.s(); !(_step = _iterator.n()).done;) {
+        var _step$value = _slicedToArray(_step.value, 2),
+          useFile = _step$value[0],
+          _index = _step$value[1];
+        var name = useFile.toString().trim();
+        var what = FileRegex.exec(name) || ['', ''];
+        switch (what[1]) {
+          case 'js':
+            mw.loader.load("".concat(path, "javascript&title=").concat(encodeURIComponent(name)));
+            break;
+          case 'css':
+            mw.loader.load("".concat(path, "css&title=").concat(encodeURIComponent(name)));
+            break;
+          default:
+            if (ExtRegex.test(name)) {
+              mw.loader.load(name);
+            }
+        }
       }
-    });
-  });
+    } catch (err) {
+      _iterator.e(err);
+    } finally {
+      _iterator.f();
+    }
+  }
+  /**
+   * Add highlight to revisions when using `&hilight=revid` or `&highlight=revid`
+   */
+  var hilight = mw.util.getParamValue('hilight');
+  var highlight = mw.util.getParamValue('highlight');
+  if (mw.config.get('wgAction') === 'history') {
+    if (hilight) {
+      hilight.split(',').forEach(function (_index, version) {
+        $("input[name=oldid][value=".concat(version, "]")).parent().addClass('not-patrolled');
+      });
+    }
+    if (highlight) {
+      highlight.split(',').forEach(function (_index, version) {
+        $("input[name=oldid][value=".concat(version, "]")).parent().addClass('not-patrolled');
+      });
+    }
+  }
   /**
    * Add target="blank" to external links
    */
-  $(function () {
-    $('a.external, a[rel="mw:ExtLink"]').filter(function () {
-      var h = String($(this).attr('href')).split('/');
-      if (h.length < 3 || h[2] === location.host) {
+  $('a.external, a[rel="mw:ExtLink"]').filter(function (_index, element) {
+    var self = element;
+    var linkHref = $(element).attr('href');
+    if (linkHref !== undefined) {
+      var hrefSplit = linkHref.split('/');
+      if (hrefSplit.length < 3 || hrefSplit[2] === location.host) {
         return false;
       }
-      if (this.href.indexOf("".concat(location.protocol, "//").concat(location.hostname)) !== 0) {
-        this.target = '_blank';
-        if (!this.rel.includes('noopener')) {
-          this.rel += ' noopener';
-        }
-        if (!this.rel.includes('noreferrer')) {
-          this.rel += ' noreferrer';
-        }
+    }
+    if (self.href.indexOf("".concat(location.protocol, "//").concat(location.hostname)) !== -1) {
+      self.target = '_blank';
+      if (!(self.rel.indexOf('noopener') !== -1)) {
+        self.rel += ' noopener';
       }
-      return true;
-    });
+      if (!(self.rel.indexOf('noreferrer') !== -1)) {
+        self.rel += ' noreferrer';
+      }
+    }
+    return true;
   });
   /**
    * Remove title=* from permalink
    */
-  $(function () {
-    var permalink = document.querySelector('#t-permalink');
-    if (permalink) {
-      permalink.firstChild.href = permalink.firstChild.href.replace(/title=[^&]*&/, '');
-    }
+  var permaLink = document.querySelector('#t-permalink');
+  if (permaLink) {
+    var permaLinkFirstChild = permaLink.firstChild;
+    permaLinkFirstChild.href = permaLinkFirstChild.href.replace(/title=[^&]*&/, '');
+  }
+  /**
+   * Open search results in a new tab or window
+   * when holding down the Ctrl key (by Timeshifter)
+   */
+  $('#searchform, #searchbox, #search, .search-types, #search-types').on('keyup keydown mousedown', function (_ref) {
+    var ctrlKey = _ref.ctrlKey,
+      metaKey = _ref.metaKey;
+    $(this).attr('target', ctrlKey || metaKey ? '_blank' : '');
   });
   /**
    * Cleanup title for all pages
    */
-  $(function () {
-    var titleCleanUp = function titleCleanUp() {
-      mw.loader.using(['mediawiki.Title'], function () {
-        var title = new mw.Title(mw.config.get('wgPageName'));
-        var wipTitle = title.toText();
-        $('.firstHeading').text(wipTitle);
+  var titleCleanUp = function titleCleanUp() {
+    var oldTitleTag = $('title').text();
+    var oldPageTitle = $('.firstHeading').text();
+    var newPageTitle = new mw.Title(mw.config.get('wgPageName')).toText();
+    $('title').text(oldTitleTag.replace(oldPageTitle, newPageTitle));
+    $('.firstHeading').text(oldPageTitle.replace(oldPageTitle, newPageTitle));
+  };
+  if (mw.config.get('wgAction') === 'view' && [2, 3, 6, 118].indexOf(mw.config.get('wgNamespaceNumber')) !== -1 && !(location.href.indexOf('diff=') !== -1)) {
+    titleCleanUp();
+  }
+  /**
+   * Display title=(.*) of <span class="inline-unihan"> after them.
+   * (beta test)
+   */
+  // Do not display on Special Pages
+  if (mw.config.get('wgNamespaceNumber') >= 0) {
+    $('attr, .inline-unihan').each(function (_index, element) {
+      var elementTitle = element.title;
+      if (!elementTitle) {
+        return;
+      }
+      var popup = new OO.ui.PopupWidget({
+        $content: $("<p>").text(elementTitle),
+        padded: true,
+        anchor: true,
+        head: true,
+        label: window.wgULS('注释：', '注釋：')
       });
-    };
-    switch (mw.config.get('wgNamespaceNumber')) {
-      case 2:
-      case 3:
-      case 6:
-      case 7:
-      case 118:
-        {
-          if (mw.config.get('wgAction') === 'view' && !location.href.includes('diff=') && !location.href.includes('oldid=')) {
-            titleCleanUp();
-          }
-          break;
-        }
-      default:
-        {
-          return;
-        }
-    }
-  });
-  /**
-   * Map addPortletLink to mw.util
-   *
-   * @deprecated: Use mw.util.addPortletLink instead.
-   */
-  mw.log.deprecate(window, 'addPortletLink', function () {
-    var _mw$util$addPortletLi;
-    return (_mw$util$addPortletLi = mw.util.addPortletLink).apply.apply(_mw$util$addPortletLi, arguments);
-  }, 'Use mw.util.addPortletLink() instead');
-  /**
-   * Extract a URL parameter from the current URL
-   *
-   * @deprecated: Use mw.util.getParamValue with proper escaping
-   */
-  mw.log.deprecate(window, 'getURLParamValue', function () {
-    var _mw$util$getParamValu;
-    return (_mw$util$getParamValu = mw.util.getParamValue).apply.apply(_mw$util$getParamValu, arguments);
-  }, 'Use mw.util.getParamValue() instead');
-  /**
-   * Test if an element has a certain class
-   *
-   * @deprecated: Use $(element).hasClass() instead.
-   */
-  mw.log.deprecate(window, 'hasClass', function (element, className) {
-    return $(element).hasClass(className);
-  }, 'Use jQuery.hasClass() instead');
+      $(element).append(popup.$element);
+      $(element).on('click', function () {
+        popup.toggle();
+      });
+    });
+  }
+  /* 修正折叠后定位变化 */
+  if (location.hash) {
+    location.href = location.hash;
+  }
+  /* 临时：禁止用户查看用户创建日志 */
+  if (mw.config.get('wgCanonicalSpecialPageName') === 'Log') {
+    $('input[name="wpfilters[]"][value=newusers]').attr('checked', 0);
+    $('input[name="wpfilters[]"][value=newusers]').parent().parent().parent().parent().remove();
+  }
+  /* 调整折叠按钮的颜色 */
+  var $toggle = $('.mw-collapsible-toggle');
+  if ($toggle.length > 0 && (_$toggle$parent$ = $toggle.parent()[0]) !== null && _$toggle$parent$ !== void 0 && _$toggle$parent$.style.color) {
+    $toggle.find('a').css('color', 'inherit');
+  }
+  /* 隐藏首页红色链接 */
+  if (mw.config.get('wgIsMainPage') && mw.config.get('wgAction') === 'view') {
+    /* Remove red links */
+    $('#mw-content-text a.new').contents().unwrap();
+  }
+});
+$(function deprecatedFunctions() {
   /**
    * maintenance: Some user scripts may be using the following deprecated functions.
    * These functions are no longer supported and should be updated to use mw.loader.getScript.
    *
    * @deprecated: User mw.loader.load() or mw.loader.getScript() instead.
    */
-  mw.log.deprecate(window, 'importScript', function (page) {
-    return mw.loader.load("".concat(mw.config.get('wgServer') + mw.config.get('wgScript'), "?title=").concat(mw.util.wikiUrlencode(page), "&action=raw&ctype=text/javascript&maxage=86400&smaxage=86400"));
+  mw.log.deprecate(window, 'importScript', function (title) {
+    mw.loader.load(mw.util.getUrl(title, {
+      action: 'raw',
+      ctype: 'text/javascript',
+      maxage: '3600',
+      smaxage: '3600'
+    }));
   }, 'Use mw.loader.load() or mw.loader.getScript() instead');
-  mw.log.deprecate(window, 'importStylesheet', function (page) {
-    return mw.loader.load("".concat(mw.config.get('wgServer') + mw.config.get('wgScript'), "?title=").concat(mw.util.wikiUrlencode(page), "&action=raw&ctype=text/css&maxage=86400&smaxage=86400"), 'text/css');
+  mw.log.deprecate(window, 'importStylesheet', function (title) {
+    mw.loader.load(mw.util.getUrl(title, {
+      action: 'raw',
+      ctype: 'text/css',
+      maxage: '3600',
+      smaxage: '3600'
+    }), 'text/css');
   }, 'Use mw.loader.load() instead');
-  mw.log.deprecate(window, 'importScriptURI', function (URL) {
-    return mw.loader.load(mw.util.wikiUrlencode(URL));
+  mw.log.deprecate(window, 'importScriptURI', function (modules) {
+    mw.loader.load(mw.util.wikiUrlencode(modules));
   }, 'Use mw.loader.load() or mw.loader.getScript() instead');
-  mw.log.deprecate(window, 'importStylesheetURI', function (URL) {
-    return mw.loader.load(mw.util.wikiUrlencode(URL), 'text/css');
+  mw.log.deprecate(window, 'importStylesheetURI', function (modules) {
+    mw.loader.load(mw.util.wikiUrlencode(modules), 'text/css');
   }, 'Use mw.loader.load() instead');
-  mw.log.deprecate(window, 'importScriptCallback', function (page, ready) {
-    return mw.loader.getScript("".concat(mw.config.get('wgServer') + mw.config.get('wgScript'), "?title=").concat(mw.util.wikiUrlencode(page), "&action=raw&ctype=text/javascript&maxage=86400&smaxage=86400"), ready);
+  mw.log.deprecate(window, 'importScriptCallback', function (title, ready) {
+    mw.loader.getScript(mw.util.getUrl(title, {
+      action: 'raw',
+      ctype: 'text/javascript',
+      maxage: '3600',
+      smaxage: '3600'
+    })).then(ready);
   }, 'Use mw.loader.load() or mw.loader.getScript() instead');
-  mw.log.deprecate(window, 'importScriptURICallback', function () {
-    return mw.loader.getScript;
+  mw.log.deprecate(window, 'importScriptURICallback', function (url, ready) {
+    mw.loader.getScript(url).then(ready);
   }, 'Use mw.loader.getScript() instead');
   /**
    * maintenance: Some user scripts may be using the following deprecated functions.
    * These functions are kept for compability.
    */
-  mw.log.deprecate(window, 'getURLParameter', function (name) {
-    return mw.util.getParamValue(name);
-  }, 'Use mw.util.getParamValue() instead');
-  mw.log.deprecate(window, 'JSConfig', {}, 'Use {} instead');
+  // mw.log.deprecate(window, 'JSConfig', {}, 'Use {} instead');
   /**
-   * wgU*S functions have been split to a seperate gadget.
+   * wgU*S functions have been split to a seperate gadget (ext.gadget.i18n).
    */
-  mw.loader.using(['ext.gadget.i18n']);
-})(jQuery, mediaWiki);
+});
 /* </nowiki> */
