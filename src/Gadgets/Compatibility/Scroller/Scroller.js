51f986bb8fbb6f4a9d664569111dc4b782b73244
ZoruaFox <96456728+ZoruaFox@users.noreply.github.com>
2023-11-27 18:09:28 +0800

    Update Scroller.js

--- src/Gadgets/Compatibility/Scroller/Scroller.js
+++ src/Gadgets/Compatibility/Scroller/Scroller.js
@@ -30,61 +30,6 @@ var Scroller = function () {
                 this.scroller_focus_id = 0;
                 this.scroller_items = document.getElementsByClassName('scroller-item');
                 this.scroller_container = document.getElementsByClassName('scroller-container');
-                this.scroller_item_length = this.scroller_items.length;
-
-                if (this.scroller_container.length == 0 || this.scroller_items.length == 0) {
-                        return 0;
-                }
-
-                this.scroller_item_width = this.scroller_items[0].offsetWidth;
-
-                this.e_last_btn = document.getElementById('scroller-last-btn');
-                this.e_next_Btn = document.getElementById('scroller-next-btn');
-
-                this.scroller_items[0].classList.add('scroller-item-now');
-
-                this.e_last_btn.addEventListener('click', function () {
-                        _this.scroll_element_intoView(-1);
-                });
-                this.e_next_Btn.addEventListener('click', function () {
-                        _this.scroll_element_intoView(1);
-                });
-        }
-
-        _createClass(Scroller, [{
-                key: 'scroll_element_intoView',
-                value: function scroll_element_intoView(jump) {
-                        var target = this.scroller_focus_id + jump;
-
-                        if (target <= 0) {
-                                target = 0;
-                        }
-                        if (target >= this.scroller_item_length) {
-                                target = this.scroller_item_length - 1;
-                        }
-
-                        this.scroller_container[0].scrollTo(this.scroller_item_width * target, 0);
-
-                        try {
-                                this.scroller_items[this.scroller_focus_id].classList.remove('scroller-item-now');
-                        } finally {}
-                        this.scroller_items[target].classList.add('scroller-item-now');
-
-                        this.scroller_focus_id = target;
-                }
-        }]);
-
-        return Scroller;
-        }();
-
-        new Scroller();
-                var _this = this;
-
-                _classCallCheck(this, Scroller);
-
-                this.scroller_focus_id = 0;
-                this.scroller_items = document.getElementsByClassName('scroller-item');
-                this.scroller_container = document.getElementsByClassName('scroller-container');
                 this.scroller_item_width = this.scroller_items[0].offsetWidth;
                 this.scroller_item_length = this.scroller_items.length;
 
